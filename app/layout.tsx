1f7341ffc46034fc60fd926b6e74875d81baa360
Julien TESSIEAU <julien.tessieau@gmail.com>
2024-01-10 19:12:12 +0000

    add stylesheet and css reset to the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 
+import "@/app/style/global.scss"
+
 const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
