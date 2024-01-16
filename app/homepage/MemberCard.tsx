69ad0507d8604bffde30c13cf155d07cd297f269
Julien TESSIEAU <julien.tessieau@gmail.com>
2024-01-16 12:58:06 +0000

    add placeholder image to MemberCard picture is not provided

--- app/homepage/MemberCard.tsx
+++ app/homepage/MemberCard.tsx
@@ -5,7 +5,8 @@ export default function MemberCard({member}: { member: Member }) {
     return (
         <div className={"member-card"}>
             <div className={"member-picture"}>
-                <Image src={member.picture} alt={"Photo de " + member.name} sizes={"100%"} width={300} height={300}/>
+                <Image src={member.picture ? member.picture : "/assets/members/default.jpg"}
+                       alt={"Photo de " + member.name} sizes={"100%"} width={300} height={300}/>
             </div>
             <div className={"member-name"}>{member.name}</div>
             <div className={"member-role"}>{member.role}</div>
