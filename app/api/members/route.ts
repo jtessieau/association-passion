4f6ceb309911729aad2371dbf511486af8bd74f8
Julien TESSIEAU <julien.tessieau@gmail.com>
2024-01-17 09:48:21 +0000

    refactor the GET and POST method for api endpoint /member
    - add try catch
    - add disconnection from database

--- app/api/members/route.ts
+++ app/api/members/route.ts
@@ -1,20 +1,93 @@
 import {PrismaClient} from "@prisma/client"
+import MemberFormValidator from "@/app/utils/validators/MemberFormValidator";
 
 const prisma = new PrismaClient()
 
 export async function GET() {
+    try {
+        const result = await prisma.members.findMany()
 
-    const result = await prisma.members.findMany()
-
-    return Response.json({
-        message: 'ok',
-        status: 200,
-        data: result
-    })
+        return Response.json({
+            message: 'ok',
+            data: result
+        }, {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } catch (error) {
+        return Response.json({
+            "error": "Oops, something wrong happened...",
+            "details": error
+        }, {
+            status: 500,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } finally {
+        await prisma.$disconnect()
+    }
 }
 
 export async function POST(request: Request) {
     const formData = await request.formData()
-    console.log(formData)
-    return Response.json("ok")
+
+    // -- Validation -- //
+    const validation = MemberFormValidator(formData)
+
+    if (!validation.isValid) {
+        return Response.json({
+            "error": "Les données reçu ne sont pas conformes.",
+            "details": validation
+        }, {
+            status: 400,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
+
+
+    // -- Sanitize -- //
+    function sanitize(userProvidedString: string) {
+        return userProvidedString.trim().replace(/</g, '&lt;').replace(/>/g, '&gt;')
+    }
+
+    const formDataArray: Member = {
+        name: sanitize(formData.get("memberName")?.toString() + ""),
+        role: sanitize(formData.get("memberRole")?.toString() + ""),
+        isActive: (formData.get("memberIsActive")?.toString() + "") == "true",
+        picture: ""
+    }
+
+    // -- Storage -- //
+    try {
+        const result = await prisma.members.create({
+            data: formDataArray
+        })
+
+        return Response.json({
+            "message": "member added",
+            "data": result
+        }, {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } catch (error) {
+        return Response.json({
+            "error": "Oops, something wrong happened...",
+            "details": error
+        }, {
+            status: 500,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } finally {
+        await prisma.$disconnect()
+    }
 }
