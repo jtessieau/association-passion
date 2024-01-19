0a09c7b0eda099939416b54a2bc00ba92d8be71f
Julien TESSIEAU <julien.tessieau@gmail.com>
2024-01-19 19:03:58 +0000

    handle delete function implemented to the view

--- app/dashboard/members/memberRow.tsx
+++ app/dashboard/members/memberRow.tsx
@@ -1,13 +1,31 @@
 "use client"
 
-function handleEdit() {
-    console.log("edit")
-}
+import {useRouter} from "next/navigation";
 
-function handleDelete() {
-}
+export default function MemberRow({member}: { member: MemberFromDatabase }) {
+    const router = useRouter()
 
-export default function MemberRow({member}: { member: Member }) {
+    function handleEdit(member: MemberFromDatabase) {
+        console.log("edit " + member.id)
+    }
+
+    async function handleDelete(member: MemberFromDatabase) {
+
+        const response = await fetch(process.env.BASE_URL + "/api/members", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(member)
+        })
+
+        if (response.status === 204) {
+            router.push('/dashboard')
+        } else {
+            console.error(response.json())
+        }
+    }
+
     return (
         <tr>
             <td>{member.name}</td>
@@ -15,11 +33,11 @@ export default function MemberRow({member}: { member: Member }) {
             <td>{member.picture}</td>
             <td>{member.isActive}</td>
             <td>
-                <button onClick={handleEdit}>edit
+                <button onClick={() => handleEdit(member)}>edit
                 </button>
             </td>
             <td>
-                <button onClick={handleDelete}>delete</button>
+                <button onClick={() => handleDelete(member)}>delete</button>
             </td>
         </tr>
     )
--- app/dashboard/members/membersTable.tsx
+++ app/dashboard/members/membersTable.tsx
@@ -1,9 +1,21 @@
+'use client'
+
 import {fetchMembers} from "@/app/utils/database/members";
 import MemberRow from "@/app/dashboard/members/memberRow";
+import {useEffect, useState} from "react";
 
-export default async function MembersTable() {
+export default function MembersTable() {
 
-    const {data: members}: { data: Member[] } = await fetchMembers()
+    const [members, setMembers] = useState<MemberFromDatabase[]>([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const {data: members}: { data: MemberFromDatabase[] } = await fetchMembers()
+            setMembers(members)
+        }
+
+        fetchData()
+    });
 
     return (
         <table>
